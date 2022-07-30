c9171c55e12bc2b193386512d21de54684ec4311
brunomont <brunoadrian.mont@gmail.com>
2022-07-30 11:27:27 -0500

    mod: useDarkMode hook

--- resources/js/Hooks/useDarkMode.jsx
+++ resources/js/Hooks/useDarkMode.jsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from 'react'
 
 export default function useDarkMode () {
-  const [theme, setTheme] = useState('dark')
+  const previousTheme = window.localStorage.getItem('theme')
+  const [theme, setTheme] = useState(previousTheme || 'light')
   const colorTheme = theme === 'dark' ? 'light' : 'dark'
 
   useEffect(() => {
